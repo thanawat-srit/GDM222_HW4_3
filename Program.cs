70d6b628439e1a9d74148b08ea5cd17e4e71f633
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-04-30 23:46:28 +0700

    Add Program

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int nNode = int.Parse(Console.ReadLine());
+        Graph g = new Graph(nNode);
+        List<int> intLists = new List<int>();
+
+        while(true){
+            bool srcCheck = true;
+            bool dstCheck = true;
+
+            int src = int.Parse(Console.ReadLine());
+            if(src < 0){
+                break;
+            }
+            foreach (int node in intLists){
+                if(node == src){
+                    srcCheck = false;
+                    break;
+                }
+            }
+            if(srcCheck){
+                intLists.Add(src);
+            }
+            if(intLists.Count > nNode){
+                break;
+            }
+
+            int dst = int.Parse(Console.ReadLine());
+            if(dst < 0){
+                break;
+            }
+            foreach (int node in intLists){
+                if(node == dst){
+                    dstCheck = false;
+                    break;
+                }
+            }
+            if(dstCheck){
+                intLists.Add(dst);
+            }
+            if(intLists.Count > nNode){
+                break;
+            }
+
+            g.AddEdge(src, dst);
+        }
+
+        if (g.IsReachable(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())))
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+    }
+}
